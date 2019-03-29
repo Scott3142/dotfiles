a50ae7fc5cdf2874d9434f6be7b5e3a3afc3d777
Scott Morgan <MorganSN@cardiff.ac.uk>
2019-03-29 13:43:22 +0000

    Update setup

--- setup.sh
+++ setup.sh
@@ -1,27 +1,16 @@
 #!/bin/bash
 
-<<<<<<< HEAD
 #sudo add-apt-repository ppa:gnome-terminator
 #sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0DF731E45CE24F27EEEB1450EFDC8610341D9410
 #echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
-=======
-sudo add-apt-repository ppa:gnome-terminator
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0DF731E45CE24F27EEEB1450EFDC8610341D9410
-echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
-curl -L https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
-sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
->>>>>>> f9bf2a75d42d549500cc1efc2b86838d97c078c5
 
 sudo apt -y update
 sudo apt -y upgrade
 sudo apt -y dist-upgrade
 sudo apt -y autoremove
 
-<<<<<<< HEAD
+sudo apt -y install feh
 sudo apt -y install py3status
-=======
-sudo pip install py3status
->>>>>>> f9bf2a75d42d549500cc1efc2b86838d97c078c5
 sudo apt -y install i3 i3status i3lock
 sudo apt -y install git
 sudo apt -y install neovim
