8ff1a8afd694a9d614b87170c89c3d16d0af5417
Scott Morgan <scott.morgan3142@gmail.com>
2020-04-08 17:48:45 +0100

    Adds updatepc file

--- setup.sh
+++ setup.sh
@@ -6,10 +6,9 @@
 #sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0DF731E45CE24F27EEEB1450EFDC8610341D9410
 #echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
 
-sudo apt -y update
-sudo apt -y upgrade
-sudo apt -y dist-upgrade
-sudo apt -y autoremove
+cp updatepc /home/scott/
+
+bash /home/scott/updatepc
 
 sudo apt -y install feh
 sudo apt -y install py3status
